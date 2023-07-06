a924efd9a302b5f3458fb472fe197b10f9ceba4f
King saozer <saozerkinghacker@gmail.com>
2023-07-06 15:26:49 +0100

    Création de la page PageGestionPraticiens et inscription du state dans REDUX

--- src/Constants/dataFields.js
+++ src/Constants/dataFields.js
@@ -1,4 +1,3 @@
-export const DATA_TABLE_PRATICIEN_COLONNE = ["Civilité", "Profession", "Nom", "Prenom", "Initials", "Actif", "Date de fin d'activité", "Est chirurgien", "Le groupe", "Lieu", "Prix defaut", "Actions"]
 export const DATA_TABLE_PATIENT_COLONNE = ["Civilité", "Nom", "Prénom", "Date de naissance", "Téléphone", "Email", "Mot de Passe", "Initials", "Photo", "Actif", "Droits", "Actions"]
 export const DATA_TABLE_LIEU_COLONNE = ["Label", "Ville", "Région", "Code Postal", "Réference", "Initials", "Coordonées geographique", "Actif", "Actions"]
 export const DATA_TABLE_MOTIF_COLONNE = ["Label", "Nom", "Temps par défaut", "Couleur Motif", "Reference", "Actif", "Actions"]
@@ -48,5 +47,31 @@ export const DATA_TABLE_USERS_COLONNE = [
         label: "Actions"
     }
 ]
+
+export const DATA_TABLE_PRATICIEN_COLONNE = [
+  {
+    label: "Civilité",
+    name: "label_civility",
+  },
+  {
+    label: "Nom",
+    name: "name",
+  },
+  {
+    label: "Prénom",
+    name: "surname",
+  },
+  {
+    label: "Email",
+    name: "email",
+  },
+  {
+    label: "Actif",
+    name: "active",
+  },
+  {
+    label: "Actions",
+  },
+];
 
 export const DATA_TABLE_GROUPE_COLONNE = ["Nom du groupe", "Description", "Actions"]
--- src/REDUX/setup/rootReducer.js
+++ src/REDUX/setup/rootReducer.js
@@ -2,13 +2,15 @@ import { combineReducers } from "redux";
 import CommonReducer from "../commons/reducers";
 import UserReducers from "../users/reducers";
 import GroupReducers from "../groups/reducer";
+import PraticiensReducer from "../praticiens/reducer";
 
 
 
 const rootReducer = combineReducers({
     Common: CommonReducer,
     Users: UserReducers,
-    Groups: GroupReducers
+    Groups: GroupReducers,
+    Praticiens: PraticiensReducer,
 })
 
 export default rootReducer;
