a2e930d7069c66a9dfda9ff8dd9ae4d8b7e8bf92
tiemeni <tiemanirocket@gmail.com>
2023-07-09 22:06:28 +0100

    ffinished signin

--- src/GlobalRouter/index.js
+++ src/GlobalRouter/index.js
@@ -11,31 +11,34 @@ import ContentRouter from "./ContentRouter";
 
 const Routeur = () => {
     const connected_user = useSelector(state => state.Common.isValidToken)
-    const token_value = localStorage.getItem("acces_bo_token")
+    const [token_value, setTokenValue] = useState(localStorage.getItem("acces_bo_token"))
     const dispatch = useDispatch()
     const [canGo, setCanGo] = useState(connected_user)
     const [loading, setLoading] = useState(true)
-    const handleRes = async () => {
+    const handleRes = () => {
         // tiemanirocksqsqetdss@gmail.com
         // qwerty123
-        const data = await isValidToken({ token: token_value })
-        dispatch(setIsConnected(data))
-        setLoading(false)
-        setCanGo(() => {
-            return data
-        });
+        // const data = await isValidToken({ token: token_value })
+        // dispatch(setIsConnected(data))
+        // setLoading(false)
+        // setCanGo(() => {
+        //     return data
+        // });
+        setTokenValue(localStorage.getItem("acces_bo_token"))
     }
 
 
     useEffect(() => {
-        // handleRes()
+        handleRes()
     }, [])
+
+    console.log(token_value)
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<AuthWrapper />} />
-                <Route path="/content/*" element={<ContentRouter />} />
+                <Route path="/" element={token_value ? <Navigate to={"/content"} /> : <AuthWrapper />} />
+                <Route path="/content/*" element={token_value ? <ContentRouter /> : <Navigate to={"/"} />} />
                 <Route path="*" element={<Navigate to={"/"} />} />
             </Routes>
         </BrowserRouter>
