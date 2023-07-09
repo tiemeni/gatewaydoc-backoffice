f8c3cf9e6a6539b2872d3814fc624698d6e81e8f
dongmodonald95 <dongmodonald95@gmail.com>
2023-07-09 16:26:40 +0100

    organisaer le calendrier

---
+++ src/Components/authers/FullCalendar/style.js
@@ -0,0 +1,12 @@
+import { createStyles } from "@mui/material";
+import { Colors } from '../../../Constants/colors';
+
+const styles = createStyles({
+    practitionerTile: {
+        fontSize: 27,
+        textAlign: 'center',
+        marginBottom: 1,
+    }
+});
+
+export default styles;
