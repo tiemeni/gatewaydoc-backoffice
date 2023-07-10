4cdbcb134dcc70fa724be57099d6a10ece55a489
tiemeni <tiemanirocket@gmail.com>
2023-07-10 12:48:17 +0100

    refact contentRouter

--- src/GlobalRouter/index.js
+++ src/GlobalRouter/index.js
@@ -1,28 +1,14 @@
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import AuthWrapper from "../Pages/AuthWrapperPages";
 import ContentRouter from "./ContentRouter";
 
 
 const Routeur = () => {
-    const connected_user = useSelector(state => state.Common.isValidToken)
     const [token_value, setTokenValue] = useState(localStorage.getItem("acces_bo_token"))
-    const dispatch = useDispatch()
-    const [canGo, setCanGo] = useState(connected_user)
-    const [loading, setLoading] = useState(true)
     const handleRes = () => {
-        // tiemanirocksqsqetdss@gmail.com
-        // qwerty123
-        // const data = await isValidToken({ token: token_value })
-        // dispatch(setIsConnected(data))
-        // setLoading(false)
-        // setCanGo(() => {
-        //     return data
-        // });
         setTokenValue(localStorage.getItem("acces_bo_token"))
     }
-
 
     useEffect(() => {
         handleRes()
