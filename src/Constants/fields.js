96eab5eb55dd414f84d2ef9d290e274e5b87846f
King saozer <saozerkinghacker@gmail.com>
2023-07-19 12:06:40 +0100

    Mise à jour de la liste des champs pour la création d'un practitioner

--- src/Constants/fields.js
+++ src/Constants/fields.js
@@ -110,137 +110,129 @@ export const userFields = {
 }
 
 export const practitionerFields = {
-    fields: [
-        {
-            id: 1,
-            label: "Civilite*",
-            name: "civility",
-            type: fieldTypes.SELECT,
-            required: true,
-            placeholder: ""
-        },
-        {
-            id: 2,
-            label: "Profession",
-            name: "job",
-            type: fieldTypes.SELECT,
-            required: false,
-            placeholder: ""
-        },
-        {
-            id: 3,
-            label: "Fonction",
-            name: "fonction",
-            type: fieldTypes.SELECT,
-            required: false,
-            placeholder: ""
-        },
-        {
-            id: 4,
-            label: "Initiales",
-            name: "initiales",
-            type: fieldTypes.TEXT,
-            required: false,
-            placeholder: "Initiales"
-        },
-        {
-            id: 5,
-            label: "Nom*",
-            name: "name",
-            type: fieldTypes.TEXT,
-            required: true,
-            placeholder: "NOM"
-        },
-        {
-            id: 6,
-            label: "Prenom*",
-            name: "surname",
-            type: fieldTypes.TEXT,
-            required: true,
-            placeholder: "Prenom"
-        },
-        {
-            id: 7,
-            label: "Téléphone*",
-            name: "phone",
-            type: fieldTypes.TEXT,
-            required: true,
-            placeholder: "Prenom"
-        },
-        {
-            id: 8,
-            label: "Email*",
-            name: "email",
-            type: fieldTypes.EMAIL,
-            required: true,
-            placeholder: "Email"
-        },
-        {
-            id: 9,
-            label: "Photo",
-            name: "picture",
-            type: fieldTypes.FILE,
-            required: false,
-            placeholder: ""
-        },
-        {
-            id: 10,
-            label: "Le groupe*",
-            name: "groups",
-            type: fieldTypes.SELECT,
-            required: true,
-            placeholder: ""
-        },
-        {
-            id: 11,
-            label: "Actif*",
-            name: "active",
-            type: fieldTypes.RADIO,
-            required: true,
-            placeholder: ""
-        },
-        {
-            id: 12,
-            label: "Date de fin d'activité",
-            name: "practitionerFilter",
-            type: fieldTypes.TEXT,
-            required: false,
-            placeholder: ""
-        },
-        {
-            id: 13,
-            label: "Est chirurgien*",
-            name: "chirurgien",
-            type: fieldTypes.RADIO,
-            required: true,
-            placeholder: ""
-        },
-        {
-            id: 14,
-            label: "Lieu affecté",
-            name: "affectation",
-            type: fieldTypes.AUTO_COMPLETE,
-            required: false,
-            placeholder: ""
-        },
-        {
-            id: 15,
-            label: "Filtre sur les motifs de rdv",
-            name: "motifFilter",
-            type: fieldTypes.AUTO_COMPLETE,
-            required: false,
-            placeholder: ""
-        },
-        {
-            id: 16,
-            label: "Prix défaut",
-            name: "defaultPrice",
-            type: fieldTypes.TEXT,
-            required: false,
-            placeholder: ""
-        },
-    ]
-}
+  fields: [
+    {
+      id: 1,
+      label: "Civilite*",
+      name: "civility",
+      type: fieldTypes.SELECT,
+      required: true,
+      placeholder: "",
+    },
+    {
+      id: 2,
+      label: "Nom*",
+      name: "name",
+      type: fieldTypes.TEXT,
+      required: true,
+      placeholder: "NOM",
+    },
+    {
+      id: 3,
+      label: "Prenom*",
+      name: "surname",
+      type: fieldTypes.TEXT,
+      required: true,
+      placeholder: "Prenom",
+    },
+    {
+      id: 4,
+      label: "Le groupe*",
+      name: "groups",
+      type: fieldTypes.SELECT,
+      required: true,
+      placeholder: "",
+    },
+    {
+      id: 5,
+      label: "Spécialté",
+      name: "job",
+      type: fieldTypes.SELECT,
+      required: true,
+      placeholder: "",
+    },
+    {
+      id: 6,
+      label: "Initiales",
+      name: "initiales",
+      type: fieldTypes.TEXT,
+      required: false,
+      placeholder: "Initiales",
+    },
+    {
+      id: 7,
+      label: "Email*",
+      name: "email",
+      type: fieldTypes.EMAIL,
+      required: true,
+      placeholder: "Email",
+    },
+    {
+      id: 8,
+      label: "Téléphone*",
+      name: "phone",
+      type: fieldTypes.TEXT,
+      required: false,
+      placeholder: "Téléphone",
+    },
+    {
+      id: 9,
+      label: "Photo",
+      name: "picture",
+      type: fieldTypes.FILE,
+      required: false,
+      placeholder: "",
+    },
+    {
+      id: 10,
+      label: "Actif*",
+      name: "active",
+      type: fieldTypes.RADIO,
+      required: true,
+      placeholder: "",
+    },
+    {
+      id: 11,
+      label: "Date de fin d'activité",
+      name: "practitionerFilter",
+      type: fieldTypes.TEXT,
+      required: false,
+      placeholder: "",
+    },
+    {
+      id: 12,
+      label: "Est chirurgien*",
+      name: "chirurgien",
+      type: fieldTypes.RADIO,
+      required: true,
+      placeholder: "",
+    },
+    {
+      id: 13,
+      label: "Lieu affecté",
+      name: "affectation",
+      type: fieldTypes.AUTO_COMPLETE,
+      required: false,
+      placeholder: "",
+    },
+    {
+      id: 14,
+      label: "Filtre sur les motifs de rdv",
+      name: "motifFilter",
+      type: fieldTypes.AUTO_COMPLETE,
+      required: false,
+      placeholder: "",
+    },
+    {
+      id: 15,
+      label: "Prix défaut",
+      name: "defaultPrice",
+      type: fieldTypes.TEXT,
+      required: false,
+      placeholder: "",
+    },
+  ],
+};
 
 export const specialityFields = {
     fields: [
