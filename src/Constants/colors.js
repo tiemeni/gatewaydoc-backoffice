849257538e4dd10df4b083e9e0eab1d0ff327b79
fez2000 <eugenefezeu@gmail.com>
2023-07-24 09:31:44 +0100

    modal first step ready

--- src/Constants/colors.js
+++ src/Constants/colors.js
@@ -4,3 +4,22 @@ export const Colors = {
   white: "white",
   gainsboro: "gainsboro"
 };
+export const Blue = {
+  100: '#DAECFF',
+  200: '#80BFFF',
+  400: '#3399FF',
+  600: '#0072E5',
+};
+
+export const Grey = {
+  50: '#F3F6F9',
+  100: '#E7EBF0',
+  200: '#E0E3E7',
+  300: '#CDD2D7',
+  400: '#B2BAC2',
+  500: '#A0AAB4',
+  600: '#6F7E8C',
+  700: '#3E5060',
+  800: '#2D3843',
+  900: '#1A2027',
+};
