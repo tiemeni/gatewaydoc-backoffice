c578de58f5555e04057c7befc3a8709c3b51f835
King saozer <saozerkinghacker@gmail.com>
2023-07-26 13:39:23 +0100

    modification des props pour attendre la liste des permissions | délocalisation des items du menu

--- src/Components/authers/EventContextMenu/index.jsx
+++ src/Components/authers/EventContextMenu/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import './EventContextMenu.css';
-
+import { items } from "./items";
 const EventContextMenu = (props) => {
-    const { calendarEvent, left, top, items, isVisible, onHideNeeded} = props;
+    const { calendarEvent, left, top, permissions, isVisible, onHideNeeded} = props;
+
+    // définissez vos actions ici
 
     const doAction = (calndEvnt, callback) => {
         callback(calndEvnt);
@@ -13,13 +15,17 @@ const EventContextMenu = (props) => {
         }} style={{display: isVisible === true ? 'block' : 'none'}}>
             <div onContextMenu={(event)=>{event.preventDefault()}} className="event-context-menu" style={{left: left ? (left - 15)+'px' : 60+'px', top: top ? (top + 15)+'px' : 60+'px' }}>
                 <ul onContextMenu={(event)=>{event.preventDefault()}}>
-                {items ? items.map(({label, icon, action})=>{
-                    return (<li className="event-context-menu-item" onClick={
-                        (event)=>{
-                            event.preventDefault();
-                            doAction(calendarEvent, action);
-                        }
-                    }><span>{icon ? icon : ''}</span> {label ? label : ''}</li>);
+                {items ? items.map((item)=>{
+                    const {label, icon, action, hasDivider} = item.config;
+                    if(permissions.includes(item.perm)){
+                        return (<li className={hasDivider ? 'hasDivider' : ''} onClick={
+                            (event)=>{
+                                event.preventDefault();
+                                doAction(calendarEvent, action);
+                            }
+                        }><span>{icon ? icon : ''}</span> {label ? label : ''}</li>);
+                    }
+                    return "";
                 }) : ''}
                 </ul>
             </div>
