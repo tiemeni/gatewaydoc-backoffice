ddc75231aba16f431f820f7f6b20fb523c40a30c
tiemeni <tiemanirocket@gmail.com>
2023-07-27 12:22:45 +0100

    reduce opacity

--- src/Pages/AuthWrapperPages/index.js
+++ src/Pages/AuthWrapperPages/index.js
@@ -5,12 +5,13 @@ import { useLocation } from "react-router-dom";
 
 function useQuery() {
   const { search } = useLocation();
-
+  
   return new URLSearchParams(search);
 }
 
 const AuthWrapper = () => {
   const idc = useQuery().get("idc")
+  console.log(idc)
   return (
     <LoginPage idc={idc} />
   )
