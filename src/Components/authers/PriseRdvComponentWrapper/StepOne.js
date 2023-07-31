c9bad17cf8d2d6ee8bc0f7beb39f7e17e2f2ecca
fez2000 <eugenefezeu@gmail.com>
2023-07-31 13:42:34 +0100

    sequential fields

--- src/Components/authers/PriseRdvComponentWrapper/StepOne.js
+++ src/Components/authers/PriseRdvComponentWrapper/StepOne.js
@@ -18,10 +18,12 @@ import ImageIcon from '@mui/icons-material/Image';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import CircularIndeterminate from "./CircularIndeterminate";
 import { useForm } from "react-hook-form";
+import { getPraticiens } from "../../../REDUX/praticiens/actions";
 
 function StepOne( { next = ()=>{}, visible= ()=>{} }){
     const [phone, setPhone] = React.useState('');
     const [level, setLevel] = React.useState(0);
+   
     const classes = styles();
     const items = [{}];
 
@@ -47,17 +49,32 @@ function StepOne( { next = ()=>{}, visible= ()=>{} }){
         }
 
     },[level]);
-    
+    const fieldsByLevel = {
+        "profession": {
+            level: 0
+        },
+        "motif": {
+            level: 1
+        },
+        "lieu": {
+            level: 2
+        },
+        "praticien": {
+            level: 3
+        }
+    }
     React.useEffect(() => {
     
-    const subscription = watch((value, { name, type }) =>{
-        if(name == "profession"){
-            if(!value){
-                setLevel(0)
-            }else if(level <= 0){
+        const subscription = watch((values, { name, type }) =>{
+            console.log(values, name, type)
+        
+            if(!values[name]){
+                
+                setLevel(fieldsByLevel[name].level)
+            }else if(level <= fieldsByLevel[name].level){
                 setLevel(level + 1);
             }
-        }
+        
     })
     return () => subscription.unsubscribe()
     }, [watch])
@@ -74,15 +91,23 @@ function StepOne( { next = ()=>{}, visible= ()=>{} }){
     }
     
     const dispatch = useDispatch();
-    const motifsList = useSelector((state) => state.Motifs.data);
-    
-    const getMotifs = async () => {
+    const motifList = useSelector((state) => state.Motifs.data);
+    const praticienList = useSelector((state) => state.Praticiens.data);
+    const getRessources = async () => {
       
-      if (motifsList && motifsList.data && motifsList.data.length > 0) return;
-      dispatch(getAllMotif());
+      if (!(motifList && motifList.data && motifList.data.length > 0)){
+        dispatch(getAllMotif());
+      }
+      if (!(praticienList && praticienList.data && praticienList.data.length > 0)){
+        dispatch(getPraticiens());
+      }
+      
     };
+    
+    
+
     useEffect(()=>{
-      getMotifs()
+        getRessources()
     },[]);
 
     
@@ -108,25 +133,25 @@ function StepOne( { next = ()=>{}, visible= ()=>{} }){
                         
                         {
                             level  >= 0 ? <Grid item xs={12}>
-                            <BasicFormControl  label="Quel est la profession ?"  Input={SelectWithOption} props={{ value: "649abbd384c9741c138bafba", options: motifsList && motifsList.data || [], value: getValues("profession"), ...register('profession',{ required: true }), placeholder: 'Nom' }} />
+                            <BasicFormControl  label="Quel est la profession ?"  Input={SelectWithOption} props={{  options: motifList && motifList.data || [], value: getValues("profession"), ...register('profession',{ required: true }), placeholder: 'Profession' }} />
                         </Grid>: []
                         }
                         {
                             level  >= 1 ?
                             <Grid item xs={12}>
-                                <BasicFormControl label="Quel est le motif de la consultation ?"  Input={SelectWithOption} props={{ name: 'name', options: motifsList && motifsList.data || [], ...register("motif"), placeholder: 'Nom' }} />
+                                <BasicFormControl label="Quel est le motif de la consultation ?"  Input={SelectWithOption} props={{ value: getValues("motif"), options: motifList && motifList.data || [], ...register("motif"), placeholder: 'Motif' }} />
                             </Grid>
                             :[]
                         }
                         {
                             level  >= 2 ? <Grid item xs={12}>
-                                <BasicFormControl label="Quel est le lieux de rendez vous ?"  Input={SelectWithOption} props={{ name: 'name', ...register("lieux"), placeholder: 'Nom' }} />
+                                <BasicFormControl label="Quel est le lieu de rendez vous ?"  Input={SelectWithOption} props={{ value: getValues("lieu"), options: motifList && motifList.data || [], ...register("lieu"), placeholder: 'Lieu du rendez vous' }} />
                             </Grid>:[]
                         }
                         {
                             level  >= 3 ?
                             <Grid item xs={12}>
-                                <BasicFormControl label="Avec quel praticien ?" Input={SelectWithOption} props={{ name: 'name', placeholder: 'Nom' }} />
+                                <BasicFormControl label="Avec quel praticien ?" Input={SelectWithOption} props={{ value: getValues("praticen"), options: motifList && motifList.data || [], ...register("praticien"), placeholder: 'Praticien' }} />
                             </Grid>
                             : []
                         }
@@ -136,19 +161,19 @@ function StepOne( { next = ()=>{}, visible= ()=>{} }){
                     {
                             level  >= 4 ? <Grid container spacing={1}>
                             <Grid item xs={4}>
-                                <BasicFormControl  label='Date de debut'  Input={StyledInput} props={{ name: 'name' , type: DATE, placeholder: 'Nom' }} />
+                                <BasicFormControl  label='Date de debut'  Input={StyledInput} props={{ value: getValues("date_debut"),...register("date_debut") , type: DATE, placeholder: 'Date debut' }} />
                             </Grid>
                             <Grid item xs={4}>
-                                <BasicFormControl  label='Jour'  Input={SelectWithOption} props={{ name: 'name', placeholder: 'Nom' }} />
+                                <BasicFormControl  label='Jour'  Input={SelectWithOption} props={{ value: getValues("jour"),...register("jour"), placeholder: 'Nom' }} />
                             </Grid>
                             <Grid item xs={4}>
-                                <BasicFormControl  label='Creneau horaire'  Input={SelectWithOption} props={{ name: 'name', placeholder: 'Nom' }} />
+                                <BasicFormControl  label='Creneau horaire'  Input={SelectWithOption} props={{ value: getValues("heure"), ...register("heure"), placeholder: 'Nom' }} />
                             </Grid>    
                             <Grid item xs={4}>
-                                <BasicFormControl label='Periode'  Input={StyledInput} props={{ name: 'name' , placeholder: 'Nom' }} />
+                                <BasicFormControl label='Periode'  Input={StyledInput} props={{ value: getValues('periode'), ...register("periode"), placeholder: 'Nom' }} />
                             </Grid>
                             <Grid item xs={4}>
-                                <BasicFormControl label='Creneau horaire'  Input={SelectWithOption} props={{ name: 'name', placeholder: 'Nom' }} />
+                                <BasicFormControl label='Creneau horaire'  Input={SelectWithOption} props={{ value: getValues('interval'), ...register("interval"), placeholder: 'Nom' }} />
                             </Grid>
                             <Grid item xs={4}>
                                 <Button className={classes.search} variant="contained"  color="primary" size="medium"  startIcon={<SearchIcon />}>
