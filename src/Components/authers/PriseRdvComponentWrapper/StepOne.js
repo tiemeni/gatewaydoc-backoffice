f36f1e0f7c13f12b8079435759904368b954bd1e
fez2000 <eugenefezeu@gmail.com>
2023-08-14 09:52:05 +0100

    time range

--- src/Components/authers/PriseRdvComponentWrapper/StepOne.js
+++ src/Components/authers/PriseRdvComponentWrapper/StepOne.js
@@ -58,6 +58,27 @@ const fieldsByLevel = {
         level: 5
     }
 }
+const MAX = 1440 * 2;
+const generateTimeRange = (timeSlot =40 )=>{
+    let ranges = [];
+    let currentTime = 0;
+    let num_page = MAX / timeSlot;
+    let i = 0;
+    let startTime = dayjs().minute(0).hour(0);
+    let endTime = dayjs().minute(0).hour(0);
+    endTime = endTime.add(timeSlot,"minute");
+    for(let i =0; i < num_page; i ++){
+//        a.add(timeSlot,"minute")
+        ranges.push({
+            value: `${startTime.format("HH:mm")}-${endTime.format("HH:mm")}`,
+            label: `De ${startTime.format("HH:mm")} a ${endTime.format("HH:mm")}`,
+        })
+        i ++;
+        startTime = endTime;
+        endTime = endTime.add(timeSlot,"minute");
+    }
+    return ranges;
+}
 const CRENEAUX = [{   
     value: "00:00-01:00",
     label: "00:00-01:00",        
